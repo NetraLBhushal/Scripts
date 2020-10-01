8e2377ab1e140c928d9baf6b3046286efd34c8c5
NetraLBhushal <68638491+NetraLBhushal@users.noreply.github.com>
2020-10-01 16:11:44 +0530

    Add files via upload

---
+++ SA.ps1
@@ -0,0 +1,36 @@
+function New-ServiceAccount {
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $samaccountname,
+         [Parameter(Mandatory=$true)]
+        [string]
+        $description,
+         [Parameter(Mandatory=$true)]
+        [string]
+        $password,
+         [Parameter(Mandatory=$true)]
+        [string]
+         $destou
+    )
+    $psw = convertto-securestring "$password" -asplaintext -force
+    New-ADUser -Path $destou -Name "$samaccountname"  -AccountPassword $psw -Enabled $true -AllowReversiblePasswordEncryption $false -CannotChangePassword $true -PasswordNeverExpires $true
+    Write-Output "$samaccountname service account created in $destou"
+}
+
+$CSVFILEPATH = "https://github.com/NetraLBhushal/DSC/raw/master/CustomScripts.zip/service_accounts.csv"
+$DEST_OU="OU=Service Accounts,DC=azure,DC=energy,DC=internal"
+
+if ((Test-path ($CSVFILEPATH)) -eq $false){
+    throw "CSV FILE $CSVFILEPATH not found!"
+}
+
+Foreach ($sa in $(import-csv -Path $CSVFILEPATH)){
+    New-ServiceAccount -samaccountname $sa.samaccountname -description $sa.description -password $sa.password -destou $DEST_OU
+}
+
+get-aduser -searchbase "OU=Service Accounts,DC=azure,DC=energy,DC=internal" -filter * | select-object samaccountname
+
+
+
