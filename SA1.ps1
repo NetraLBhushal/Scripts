d37cc054e83b248fdb871f93274483b72c75bad0
NetraLBhushal <68638491+NetraLBhushal@users.noreply.github.com>
2020-10-05 16:58:42 +0530

    Update SA1.ps1

--- SA1.ps1
+++ SA1.ps1
@@ -1,6 +1,6 @@
 Param ($SAtestuser, $SAtestpass, $description)
 $SAtestuser = $SAtestuser.Split(",")
-$SAtestpass = $SAtestpass.Split(",")
+$SAtestpass = $SAtestpass.Split(" ")
 $description = $description.Split(",")
 
 function New-ServiceAccount {
@@ -23,7 +23,6 @@ function New-ServiceAccount {
     New-ADUser -Path $destou -Name "$samaccountname"  -AccountPassword $psw -Enabled $true -AllowReversiblePasswordEncryption $false -CannotChangePassword $true -PasswordNeverExpires $true
     Write-Output "$samaccountname service account created in $destou"
 }
-
 
 #$CSVFILEPATH = "$pwd\Service_Account.csv"
 $DEST_OU="OU=Service Accounts,DC=azure,DC=energy,DC=internal"
