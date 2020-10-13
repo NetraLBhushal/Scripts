467bd75cc66918ff30af5cc176bd6a155819d4ea
NetraLBhushal <68638491+NetraLBhushal@users.noreply.github.com>
2020-10-13 14:59:22 +0530

    Update SA.ps1

--- SA.ps1
+++ SA.ps1
@@ -25,7 +25,7 @@ function New-ServiceAccount {
     Write-Output "$samaccountname service account created in $destou"
 }
 
-$DEST_OU="OU=Service Accounts,DC=azure,DC=energy,DC=internal"
+$DEST_OU="OU=Service,DC=azure,DC=energy,DC=internal"
 for($i = 0; $i -lt $SAtestuser.length; $i++) { 
 
     New-ServiceAccount -samaccountname $SAtestuser[$i] -description $description[$i] -password $SAtestpass[$i] -destou $DEST_OU
